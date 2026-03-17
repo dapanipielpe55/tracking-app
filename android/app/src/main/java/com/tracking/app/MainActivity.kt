a47ccca44303205517787e13f6c5e736034d09e4
dapanipielpe55 <dapanipielpe55@gmail.com>
2026-03-17 17:47:21 -0500

    Add files via upload

---
+++ android/app/src/main/java/com/tracking/app/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.tracking.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl("https://tracking.quimiforen.com/pwa/?vehiculo_id=1")
+
+        setContentView(webView)
+    }
+}
